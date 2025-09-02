74593d16af4c22151618cedcb8a7464513980636
kid005 <kid005@stge.org.uk>
2025-09-02 12:47:33 +0000

    parallelarray

--- data flow/AdvHobbies.py
+++ data flow/AdvHobbies.py
@@ -1,2 +1,34 @@
-with open("data flow/Fun_Users.csv", "r") as file:
-    print(file)
+import csv
+
+def readfile():
+    with open("data flow/Fun_Users.csv", "r") as file:
+        name = []
+        hobby = []
+        pokemon = []
+        fallout = []
+        age = []
+        job = []
+        reader = csv.reader(file) 
+        for row in reader: 
+            
+            name.append(row[0])
+            hobby.append(row[1])
+            pokemon.append(row[2])
+            fallout.append(row[3])
+            age.append(row[4])
+            job.append(row[5])
+
+        print(name)
+        print(hobby)
+        print(pokemon)
+        print(fallout)
+        print(age)
+        print(job)
+    return pokemon
+
+
+def pokecount(pokemon):
+    if 
+
+
+globalpokemon = readfile()
