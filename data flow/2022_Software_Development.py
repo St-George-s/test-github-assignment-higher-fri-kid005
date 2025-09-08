1682d165113f13353a0ed23e5b37ee7411f30532
kid005 <kid005@stge.org.uk>
2025-09-08 14:49:40 +0000

    software yippee

---
+++ data flow/2022_Software_Development.py
@@ -0,0 +1,25 @@
+import csv
+
+def new_member():
+    first_names = input("GIVE ME UR FIRST NAME: ")
+    surnames = input("GIVE ME UR LAST NAME: ")
+    categories = input("JUNIOR, ADULT, OR SENIOR: ") 
+    passwords = input("GIVE ME UR PASSWORD: ")
+    return first_names, surnames, categories, passwords 
+     
+def read_file (first_names, surnames, categories, passwords):
+    with open("data flow/members.csv", "r") as file:   
+        first_names = []
+        surnames = []
+        categories = []
+        passwords = []
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader: 
+            first_names.append(row[0])
+            surnames.append(row[1])
+            categories.append(row[2])
+            passwords.append(row[3])
+    return categories
+
+def total_members (categories):
