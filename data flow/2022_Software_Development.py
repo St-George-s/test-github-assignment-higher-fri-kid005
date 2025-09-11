8a159a545fadc98727496c9d16bd38780443972e
kid005 <kid005@stge.org.uk>
2025-09-11 09:26:40 +0000

    function tings

--- data flow/2022_Software_Development.py
+++ data flow/2022_Software_Development.py
@@ -9,12 +9,11 @@ def new_member():
      
 def read_file (first_names, surnames, categories, passwords):
     with open("data flow/members.csv", "r") as file:   
-        first_names = []
-        surnames = []
-        categories = []
-        passwords = []
+        first_names = [first_names]
+        surnames = [surnames]
+        categories = [categories]
+        passwords = [passwords]
         reader = csv.reader(file)
-        next(reader)
         for row in reader: 
             first_names.append(row[0])
             surnames.append(row[1])
@@ -31,9 +30,19 @@ def total_members (categories):
     print(counter)
 
 
-def valid_password():
-    
+def valid_password(passwords):
+    valid=False
+    while not valid:
+        password = input("Enter a password: ")
+        # ord changes characters into ASCII, chr changes ASCII into characters. [0] is the first character in the password inputted, [-1] is the last character of the password inputted.
+        if ord(password[0]) >= 65 and ord(password[0])<= 90 and ord(password[-1]) >= 35 and ord(password[-1]) <= 37:
+            print("Valid password.")
+            valid = True
+        else:
+            print("Invalid password. Please input a new password.")
+            valid = False
 
 globalnames, globalsurnames, globalcategories, globalpasswords = new_member()
 globalcategories = read_file(globalnames, globalsurnames, globalcategories, globalpasswords)
 total_members(globalcategories)
+valid_password(globalpasswords)
