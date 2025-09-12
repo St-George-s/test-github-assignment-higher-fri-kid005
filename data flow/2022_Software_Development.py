7137109b66504bb82e336309d5810ae1d248a345
kid005 <kid005@stge.org.uk>
2025-09-12 21:41:09 +0000

    commenting

--- data flow/2022_Software_Development.py
+++ data flow/2022_Software_Development.py
@@ -1,5 +1,6 @@
 import csv
 
+# Gets new member first name, second name, category, password and returns them. 
 def new_member():
     first_names = input("GIVE ME UR FIRST NAME: ")
     surnames = input("GIVE ME UR LAST NAME: ")
@@ -7,6 +8,7 @@ def new_member():
     passwords = input("GIVE ME UR PASSWORD: ")
     return first_names, surnames, categories, passwords 
      
+# opens members.csv and adds new member data into parallel arrays, returning categories.
 def read_file (first_names, surnames, categories, passwords):
     with open("data flow/members.csv", "r") as file:   
         first_names = [first_names]
@@ -21,6 +23,8 @@ def read_file (first_names, surnames, categories, passwords):
             passwords.append(row[3])
     return categories
 
+
+# Finds and displays the total amount of members in each category.
 def total_members (categories):
     search = input("What membership to count? ")
     counter = 0
@@ -29,7 +33,7 @@ def total_members (categories):
             counter = counter + 1
     print(counter)
 
-
+# Makes sure the user inputs a valid password and tells them it's invalid if it's wrong.
 def valid_password(passwords):
     valid=False
     while not valid:
@@ -42,6 +46,8 @@ def valid_password(passwords):
             print("Invalid password. Please input a new password.")
             valid = False
 
+
+# main program.
 globalnames, globalsurnames, globalcategories, globalpasswords = new_member()
 globalcategories = read_file(globalnames, globalsurnames, globalcategories, globalpasswords)
 total_members(globalcategories)
