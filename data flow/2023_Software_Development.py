ac47e3df6ef4268627c89a942ee0f5503bcb93c3
kid005 <kid005@stge.org.uk>
2025-09-16 12:42:08 +0000

    atttractions

--- data flow/2023_Software_Development.py
+++ data flow/2023_Software_Development.py
@@ -1,5 +1,6 @@
 import csv 
 
+# opens attractions.csv, adds parallel arrays and returns them.
 def read_file():
     with open("data flow/attractions.csv", "r") as file:
         attractions = []
@@ -9,5 +10,22 @@ def read_file():
         height = []
     return attractions, categories, visitors, daysOpen, height
 
-def vistited_attract(attraction, visitors):
-    
+# Finds and displays least visited attractions.
+def least_attract(attractions, visitors):
+     minimum_value = visitors[0] 
+     for x in range(1, len(visitors)):
+         if visitors[x] < minimum_value: 
+             minimum_value = visitors[x] 
+
+# Finds and displays most visited attractions.
+def most_attract(attractions, visitors):
+     maximum_value = visitors[0] 
+     for x in range(1, len(visitors)):
+         if visitors[x] > maximum_value: 
+             maximum_value = visitors[x] 
+
+
+# main program
+globalattractions, globalcategories, globalvisitors, globaldays, globalheight = read_file
+least_attract(globalattractions, globalvisitors)
+most_attract(globalattractions, globalvisitors)
