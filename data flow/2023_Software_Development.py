c6768d34aeb634e46032b6beb6a9f76ecd049abb
kid005 <kid005@stge.org.uk>
2025-09-22 14:51:00 +0000

    services

--- data flow/2023_Software_Development.py
+++ data flow/2023_Software_Development.py
@@ -8,24 +8,43 @@ def read_file():
         visitors = []
         daysOpen = []
         height = []
+        reader = csv.reader(file)
+        for row in reader:
+            attractions.append(row[0])
+            categories.append(row[1])
+            visitors.append(int(row[2]))
+            daysOpen.append(int(row[3]))
+            height.append(row[4])
     return attractions, categories, visitors, daysOpen, height
 
-# Finds and displays least visited attractions.
-def least_attract(attractions, visitors):
-     minimum_value = visitors[0] 
-     for x in range(1, len(visitors)):
-         if visitors[x] < minimum_value: 
-             minimum_value = visitors[x] 
+# Finds and displays least and most visited attractions.
+def mostleast_attract(attractions, visitors):
+    minimum_index = 0
+    maximum_index = 0
+    
+    for x in range(len(visitors)):
+        if visitors[x] < visitors[minimum_index]:
+            minimum_index = x
+        if visitors[x] > visitors[maximum_index]: 
+            maximum_index = x
+    
+    print("Most visited attraction is " + attractions[maximum_index]) 
+    print("Least visited attraction is " + attractions[minimum_index]) 
+    
 
-# Finds and displays most visited attractions.
-def most_attract(attractions, visitors):
-     maximum_value = visitors[0] 
-     for x in range(1, len(visitors)):
-         if visitors[x] > maximum_value: 
-             maximum_value = visitors[x] 
-
+# Writes the names of roller coasters that need a service within 7 days.
+def service(attractions, categories, daysOpen):
+    # 3.1 Create ‘service.csv’ file
+    with open("data flow/service.csv", "w") as file:
+    # 3.2 Loop for each attraction
+        for x in range(len(categories)):
+            if categories[x] == "Roller Coaster":
+                days = daysOpen[x]%90
+                if (90 - days) <= 7:
+                    file.write(attractions[x])
+    
 
 # main program
-globalattractions, globalcategories, globalvisitors, globaldays, globalheight = read_file
-least_attract(globalattractions, globalvisitors)
-most_attract(globalattractions, globalvisitors)
+globalattractions, globalcategories, globalvisitors, globaldays, globalheight = read_file()
+mostleast_attract(globalattractions, globalvisitors)
+service(globalattractions, globalcategories, globaldays)
