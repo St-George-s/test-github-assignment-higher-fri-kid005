29241ac87582a622e812d9780d9b294a95405f06
kid005 <kid005@stge.org.uk>
2025-09-26 12:00:38 +0000

    jhewgfwifuvewhvf

--- data flow/2023_Software_Development.py
+++ data flow/2023_Software_Development.py
@@ -22,7 +22,7 @@ def mostleast_attract(attractions, visitors):
     minimum_index = 0
     maximum_index = 0
     
-    for x in range(len(visitors)):
+    for x in range(1, len(visitors)):
         if visitors[x] < visitors[minimum_index]:
             minimum_index = x
         if visitors[x] > visitors[maximum_index]: 
@@ -37,14 +37,23 @@ def service(attractions, categories, daysOpen):
     # 3.1 Create ‘service.csv’ file
     with open("data flow/service.csv", "w") as file:
     # 3.2 Loop for each attraction
+        writer = csv.writer(file)
         for x in range(len(categories)):
             if categories[x] == "Roller Coaster":
                 days = daysOpen[x]%90
                 if (90 - days) <= 7:
-                    file.write(attractions[x])
+                    writer.writerow([attractions[x]])
+
+def height_restriction(height):
+    counter = 0
+    for x in range(len(height)):
+        if height[x][0] == "1":
+            counter += 1
+    print(counter)
     
 
 # main program
 globalattractions, globalcategories, globalvisitors, globaldays, globalheight = read_file()
 mostleast_attract(globalattractions, globalvisitors)
 service(globalattractions, globalcategories, globaldays)
+height_restriction(globalheight)
