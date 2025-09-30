e872c6a4b999ae467786545a33e65f6a21374dc8
kid005 <kid005@stge.org.uk>
2025-09-30 12:49:11 +0000

    save it fast

--- data flow/PartBPractise.py
+++ data flow/PartBPractise.py
@@ -1,11 +1,14 @@
 import csv
 
+# Record
 class Order:
      def __init__(self, name, product, amount_spent):
           self.name = name
           self.product = product
           self.amount_spent = amount_spent
 
+
+# Function that reads data from the CSV file into an array of records
 def readOrders():
     orders = []
     with open("data flow/orders.csv", "r") as file:
@@ -13,16 +16,19 @@ def readOrders():
         next(reader)
         for row in reader:
              newOrder = Order(row[0], row[1], float(row[2]))
-             orders.append(newOrder)
-   
+             orders.append(newOrder)   
     return orders
 
+
+# Procedure that finds the maximum price spent on a TV purchase
 def findMaxOrder(orders):
      maxOrder = orders[0]
      for order in orders:
-          if "TV" in order.name and order.price > maxOrder:
+          if "TV" in order.product and order.amount_spent > maxOrder.amount_spent:
                maxOrder = order
-     print(maxOrder)
+     print(maxOrder.amount_spent)
 
+
+# main program
 orders = readOrders()
-findMaxOrder(orders)
+findMaxOrder(orders) 
