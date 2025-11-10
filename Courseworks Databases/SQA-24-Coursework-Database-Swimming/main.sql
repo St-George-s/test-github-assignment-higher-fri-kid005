d50946476a161b28ce909e709b550da18dbae8f4
kid005 <kid005@stge.org.uk>
2025-11-10 15:50:01 +0000

    COMMIT COMMIY

--- Courseworks Databases/SQA-24-Coursework-Database-Swimming/main.sql
+++ Courseworks Databases/SQA-24-Coursework-Database-Swimming/main.sql
@@ -1,1 +1,23 @@
-DESCRIBE ALL TABLES;
+-- DESCRIBE ALL TABLES;
+
+-- QUESTION 2C:
+-- SELECT s.initial, s.surname, s.swimCategory, t.teamName, COUNT(r.position) AS "Races Won"
+-- FROM Swimmer S
+-- JOIN Result r ON s.swimmerID = r.swimmerID
+-- JOIN Team t ON t.teamRef = s.teamRef
+-- WHERE r.position = 1
+-- GROUP BY s.swimmerID
+-- ORDER BY s.initial ASC;
+
+-- QUESTION 2D:
+SELECT s.initial, s.surname, t.teamName, E.city, E.eventDate
+FROM Event e
+JOIN Race ra ON e.eventID = ra.eventID
+JOIN Result res ON res.raceNumber = ra.raceNumber
+JOIN Swimmer s ON res.swimmerID = s.swimmerID
+JOIN Team t ON t.teamRef = s.teamRef
+WHERE res.raceTime = (
+    SELECT MIN(res.raceTime)
+    FROM Result
+    WHERE res.lane = 1 or 8
+);
