5fa0c7a2c51eb6139e3dcde5c5bc1d579958c7ec
kid005 <kid005@stge.org.uk>
2025-12-01 15:29:37 +0000

    test done

--- Database - Database Test/main.sql
+++ Database - Database Test/main.sql
@@ -15,3 +15,11 @@ DESCRIBE ALL TABLES;
 -- UPDATE Timeslot 
 -- SET endTime = '19:30'
 -- WHERE showID = 2 AND airDate = '2024-12-24';
+
+-- PART C:
+-- SELECT s.showName, COUNT(v.ratingID) AS 'TotalRatings'
+-- FROM Show s 
+-- JOIN Episode e ON e.showID = s.showID
+-- JOIN ViewerRating v ON v.episodeID = e.episodeID
+-- GROUP BY s.showName
+-- ORDER BY COUNT(v.ratingID) DESC;
