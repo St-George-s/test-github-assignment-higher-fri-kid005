d29cbefe77c05e722da09ca3e347be1c5c0ed2cb
kid005 <kid005@stge.org.uk>
2026-02-03 09:17:04 +0000

    okay

--- Courseworks Databases/SQA-Sample-Coursework-Database/main.sql
+++ Courseworks Databases/SQA-Sample-Coursework-Database/main.sql
@@ -1,3 +1,5 @@
+-- QUESTION 2C
+
 -- SELECT c.firstName, c.surname, v.voucherID, ROUND(co.quantityPurchased * v.price) AS "Amount of Money Spent on Voucher £"
 -- FROM Customer c
 -- JOIN CustomerOrder co ON c.custID = co.custID
@@ -8,8 +10,23 @@
 -- ORDER BY v.voucherID ASC;
 
 
+-- QUESTION 2D
+
 -- SELECT v.voucherID, s.supplierName, v.voucherName, v.quantityAvailable - SUM(co.quantityPurchased) AS "Still Available"
 -- FROM Voucher v 
 -- JOIN Supplier s ON v.supplierCode = s.supplierCode
 -- JOIN CustomerOrder co ON v.voucherID = co.voucherID
 -- WHERE v.voucherID = "V543";
+
+
+-- QUESTION 2E
+
+-- SELECT s.supplierName, v.voucherName, v.price, COUNT(co.custID) AS "Number of Customers"
+-- FROM Supplier s
+-- JOIN Voucher v ON s.supplierCode = v.supplierCode
+-- JOIN CustomerOrder co ON v.voucherID = co.voucherID
+-- WHERE v.category = "Family" 
+-- AND v.price < 15
+-- GROUP BY v.voucherName
+-- ORDER BY COUNT(co.custID) DESC;
+
