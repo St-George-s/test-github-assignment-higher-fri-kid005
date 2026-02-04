14bbf724a910cd5bb106ec21628755b55c577469
kid005 <kid005@stge.org.uk>
2026-02-04 15:25:07 +0000

    we already did this lowk

--- Software/2020Coursework.py
+++
@@ -1,6 +0,0 @@
-def getMember():
-    pass
-
-
-def readFile(firstName, surname, category, password):
-    pass
---
+++ Software/Parallel arrays/2020Coursework.py
@@ -0,0 +1,28 @@
+def checkPassword():
+    valid = False
+    while valid == False:
+        password = input("enter a password: ")
+        if password[0]
+
+
+def getMember():
+    firstName = input("name: ")
+    surname = input("second name: ")
+    category = input("what category: ")
+
+    checkPassword()
+
+    return firstName, surname, category, password
+
+
+
+
+def readFile(firstName, surname, category, password):
+    pass
+
+    return category
+
+
+
+def numMembers(category):
+    pass
