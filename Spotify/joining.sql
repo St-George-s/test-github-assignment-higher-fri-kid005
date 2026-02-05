92a18e38c2d0681dd4eb5a6b222b0daa34a73f52
kid005 <kid005@stge.org.uk>
2026-02-05 10:29:34 +0000

    uhhh sure ig

--- Spotify/joining.sql
+++ Spotify/joining.sql
@@ -1,10 +1,10 @@
--- SELECT * 
--- FROM Tracks 
--- WHERE album_id IN (
---   SELECT album_id 
---   FROM Albums 
---   WHERE release_year > 2018
---  );
+SELECT * 
+FROM Tracks 
+WHERE album_id IN (
+  SELECT album_id 
+  FROM Albums 
+  WHERE release_year > 2018
+ );
 
 --  SELECT T.track_name, A.album_name 
 --  FROM Tracks T, Albums A
