2a0e1f3b264253d52eaa75a79aff381b3ab1fab6
kid005 <kid005@stge.org.uk>
2026-03-02 15:49:55 +0000

    coursework working on database part b

--- Courseworks Databases/Coursework 2026/Coursework 2026/Database/main.sql
+++ Courseworks Databases/Coursework 2026/Coursework 2026/Database/main.sql
@@ -1,1 +1,28 @@
-DESCRIBE ALL TABLES;
+-- Question 2c
+SELECT c.forename, c.surname
+FROM Customer c 
+JOIN Purchase p ON c.customerID = p.customerID
+JOIN Movie m ON p.movieCode = m.movieCode
+WHERE m.duration = (
+    SELECT MIN(duration)
+    FROM Movie m
+);
+
+
+-- Question 2d
+SELECT c.forename, c.surname, SUM(m.price) AS "Total spent(£)"
+FROM Customer c 
+JOIN Purchase p ON c.customerID = p.customerID
+JOIN Movie m ON p.movieCode = m.movieCode
+JOIN Genre g ON m.genreID = m.genreID
+WHERE g.genreName = 'Comedy'
+GROUP BY c.customerID
+ORDER BY SUM(m.price) DESC;
+
+
+-- Question 2e
+-- SELECT forename, surname, email
+-- FROM customer, purchase, movie
+-- WHERE customer.customerID = purchase.customerID
+-- AND movie.movieCode = purchase.movieCode
+-- AND released = 1990;
